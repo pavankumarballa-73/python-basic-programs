782e92c388e27b712334af7ee8f6a7cd3407ffd9
pavankumarballa-73 <pavankumarballa47@gmail.com>
2026-02-14 14:35:20 +0530

    Create program1.py

---
+++ program1.py
@@ -0,0 +1,10 @@
+i=0
+while i<3:
+  password=input("enter password:")
+  if password="secret123":
+  print(f"password:{password}")
+  break
+i+=1
+print(f"wrong!{3-i} attemps remaining")
+if i>=:
+  print("account locked")
